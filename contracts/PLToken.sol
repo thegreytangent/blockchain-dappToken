3ba54ee5811dcd1c6103e4c3b8a741c79348dcb0
thegreytangent <thegreytangent@gmail.com>
2021-09-06 16:28:34 +0800

    Handles delegated token transfer

--- contracts/PLToken.sol
+++ contracts/PLToken.sol
@@ -1,24 +1,30 @@
 pragma solidity ^0.5.16;
 
 contract PLToken {
-    
     uint256 public totalSupply;
     string public name = "PLToken";
     string public symbol = "PLCOIN";
 
-   event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
-    mapping( address => uint) public balanceOf;
+    mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    constructor(uint256 _initialSupply) public  {
+    constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
 
-    function transfer(address _to, uint _value) public returns (bool success){
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
         require(balanceOf[msg.sender] >= _value);
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
@@ -27,37 +33,35 @@ contract PLToken {
         return true;
     }
 
-    function approve(address _spender, uint256 _value) public returns(bool success) {
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
         allowance[msg.sender][_spender] = _value;
-        
         emit Approval(msg.sender, _spender, _value);
-        
         return true;
     }
 
-    
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        //Check if enough tokens;
+        require(_value <= balanceOf[_from]);
 
+        //Check allowance is big enough
+        require(_value <= allowance[_from][msg.sender]);
 
+        //Change balance;
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
 
+        //Update allowance;
+        allowance[_from][msg.sender] -= _value;
 
-
-
-
-
-
-    
-
-
-
-
-
-
-   
-
-
-
-
-  
-
-
+        //Transfer event;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
 }
--- test/PLToken.js
+++ test/PLToken.js
@@ -1,6 +1,6 @@
 let PLToken = artifacts.require("PLToken");
 
-contract('PLToken', (account) =>{
+contract('PLToken', (account) => {
     var instance;
 
     it("Init contract correct values", () => {
@@ -21,8 +21,8 @@ contract('PLToken', (account) =>{
             instance = i;
             return instance.totalSupply();
         }).then(supply => {
-           assert.equal(supply.toNumber(), 100000);
-           return instance.balanceOf(account[0]);
+            assert.equal(supply.toNumber(), 100000);
+            return instance.balanceOf(account[0]);
         }).then(adminBalance => {
             assert.equal(adminBalance.toNumber(), 100000);
         });
@@ -33,11 +33,11 @@ contract('PLToken', (account) =>{
             instance = i;
             return instance.transfer.call(account[1], 9999999999999999999);
         }).then(assert.fail).catch(err => {
-            assert( err.message.indexOf('overflow') >= 0 , 'error must contain revert');
-            return instance.transfer.call(account[1], 2500, {from: account[0]});
+            assert(err.message.indexOf('overflow') >= 0, 'error must contain revert');
+            return instance.transfer.call(account[1], 2500, { from: account[0] });
         }).then(success => {
             assert.equal(success, true, "It should be true");
-            return instance.transfer(account[1], 2500, {from: account[0]});
+            return instance.transfer(account[1], 2500, { from: account[0] });
         }).then(receipt => {
             assert.equal(receipt.logs[0].event, "Transfer");
             assert.equal(receipt.logs[0].args._from, account[0]);
@@ -69,6 +69,48 @@ contract('PLToken', (account) =>{
     })
 
 
+    it('Handles delegated token transfer ', () => {
+        return PLToken.deployed().then((i) => {
+            token_instance = i;
+            fromAccount = account[2];
+            toAccount = account[3];
+            spendingAccount = account[4];
+            return token_instance.transfer(fromAccount, 100, { from: account[0] })
+        }).then(receipt => {
+            // console.log("reciept ", receipt);
+            return token_instance.approve(spendingAccount, 10, {from: fromAccount});
+        }).then(receipt => {
+            return token_instance.transferFrom(fromAccount, toAccount,9999,{from: spendingAccount})
+        }).then(assert.fail).catch(err => {
+            assert(err.message.indexOf('revert') >= 0, 'error must contain revert');
+            return token_instance.transferFrom(fromAccount, toAccount, 20, {from : spendingAccount});
+        }).then(assert.fail).catch(err => {
+            assert(err.message.indexOf('revert') >= 0, 'error must contain revert');
+            return token_instance.transferFrom.call(fromAccount, toAccount,10,{from: spendingAccount})
+        }).then(success => {
+            assert.equal(success, true);
+            return token_instance.transferFrom(fromAccount, toAccount,10,{from: spendingAccount})
+        }).then(receipt => {
+            assert.equal(receipt.logs.length, 1, "triggers one event");
+            assert.equal(receipt.logs[0].event, "Transfer");
+            assert.equal(receipt.logs[0].args._from, fromAccount);
+            assert.equal(receipt.logs[0].args._to, toAccount);
+            assert.equal(receipt.logs[0].args._value, 10);
+            return  token_instance.balanceOf(fromAccount);
+        }).then(balance => {
+            assert.equal(balance.toNumber(), 90);
+            return  token_instance.balanceOf(toAccount);
+        }).then(balance => {
+            assert.equal(balance.toNumber(), 10);
+            return  token_instance.allowance(fromAccount, spendingAccount);
+        }).then(allowance =>{
+            assert.equal(allowance.toNumber(), 0);
+            
+        });
+    })
+
 
 
 });
+
+
